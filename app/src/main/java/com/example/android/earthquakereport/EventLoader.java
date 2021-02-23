75aa6b87cc67a894e9f324840c981af8197a20b2
Ashwath <vaashwath@gmail.com>
2021-02-23 23:21:18 +0530

    Shifted the Threading from AsyncTask to Loader

---
+++ app/src/main/java/com/example/android/earthquakereport/EventLoader.java
@@ -0,0 +1,45 @@
+package com.example.android.earthquakereport;
+
+import android.content.Context;
+import android.content.AsyncTaskLoader;
+
+import java.util.List;
+
+public class EventLoader extends AsyncTaskLoader<List<Event>> {
+
+    /** Tag for log messages */
+    private static final String LOG_TAG = EventLoader.class.getName();
+
+    /** Query URL */
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EventLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public EventLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<Event> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Event> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
--- app/src/main/java/com/example/android/earthquakereport/MainActivity.java
+++ app/src/main/java/com/example/android/earthquakereport/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.earthquakereport;
 
+
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -19,18 +24,51 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<Event>> {
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
 
     public static final String LOG_TAG = MainActivity.class.getName();
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=2&limit=30";
     private EventAdapter mAdapter;
 
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+
+
+        ConnectivityManager conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = conMgr.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()){
+
+        // Get a reference to the LoaderManager, in order to interact with loaders.
+        LoaderManager loaderManager = getLoaderManager();
+
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+        // because this activity implements the LoaderCallbacks interface).
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        }else{
+
+            View loading = findViewById(R.id.loading_circle);
+            loading.setVisibility(View.GONE);
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+
+        }
 
 
         // Find a reference to the {@link ListView} in the layout
@@ -50,60 +88,41 @@ public class MainActivity extends AppCompatActivity {
 
                 Uri earthquakeUrl = Uri.parse(currentEarthquake.getUrl());
 
-                Intent websiteIntent = new Intent(Intent.ACTION_VIEW,earthquakeUrl);
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUrl);
 
                 startActivity(websiteIntent);
             }
         });
 
-        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
-
+        earthquakeListView.setEmptyView(mEmptyStateTextView);
 
     }
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then
-     * update the UI with the list of earthquakes in the response.
-     *
-     * AsyncTask has three generic parameters: the input type, a type used for progress updates, and
-     * an output type. Our task will take a String URL, and return an Earthquake. We won't do
-     * progress updates, so the second generic is just Void.
-     *
-     * We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
-     * The doInBackground() method runs on a background thread, so it can run long-running code
-     * (like network activity), without interfering with the responsiveness of the app.
-     * Then onPostExecute() is passed the result of doInBackground() method, but runs on the
-     * UI thread, so it can use the produced data to update the UI.
-     */
-    private class EarthquakeAsyncTask extends AsyncTask<String,Void, List<Event>>{
-        @Override
-        protected List<Event> doInBackground(String... urls) {
-            // Don't perform the request if there are no URLs, or the first URL is null.
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
 
-            List<Event> result = QueryUtils.fetchEarthquakeData(urls[0]);
-            return result;
-
-        }
-        /**
-         * This method runs on the main UI thread after the background work has been
-         * completed. This method receives as input, the return value from the doInBackground()
-         * method. First we clear out the adapter, to get rid of earthquake data from a previous
-         * query to USGS. Then we update the adapter with the new list of earthquakes,
-         * which will trigger the ListView to re-populate its list items.
-         */
-
-        @Override
-        protected void onPostExecute(List<Event> data) {
-            mAdapter.clear();
-
-            if(data != null && !data.isEmpty() ){
-                mAdapter.addAll(data);
-            }
-
-        }
+    @Override
+    public Loader<List<Event>> onCreateLoader(int id, Bundle bundle) {
+        return new EventLoader(this, USGS_REQUEST_URL);
     }
 
+    @Override
+    public void onLoadFinished(Loader<List<Event>> loader, List<Event> earthquakes) {
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (earthquakes != null && !earthquakes.isEmpty()) {
+            mAdapter.addAll(earthquakes);
+        }
+        // Set empty state text to display "No earthquakes found."
+        mEmptyStateTextView.setText(R.string.no_earthquakes);
+
+        ProgressBar loading = findViewById(R.id.loading_circle);
+        loading.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Event>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+    }
 }
